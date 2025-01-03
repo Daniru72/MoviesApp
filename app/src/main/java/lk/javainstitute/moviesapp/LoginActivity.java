682d7e87570ac777a507273cca86c3bd215b9b19
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-01-03 23:13:08 +0530

    Design Register page

--- app/src/main/java/lk/javainstitute/moviesapp/LoginActivity.java
+++ app/src/main/java/lk/javainstitute/moviesapp/LoginActivity.java
@@ -1,6 +1,10 @@
 package lk.javainstitute.moviesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,17 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView gotoSignUp = findViewById(R.id.textView4);
+        gotoSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/lk/javainstitute/moviesapp/SignupActivity.java
+++ app/src/main/java/lk/javainstitute/moviesapp/SignupActivity.java
@@ -3,7 +3,7 @@ package lk.javainstitute.moviesapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class IntroActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_intro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ConstrainLayout), (v, insets) -> {
+        setContentView(R.layout.activity_signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button getStartBtn = findViewById(R.id.button);
-        getStartBtn.setOnClickListener(new View.OnClickListener() {
+
+        TextView gotoSignin = findViewById(R.id.textView10);
+        gotoSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(IntroActivity.this,LoginActivity.class);
+                Intent i = new Intent(SignupActivity.this,LoginActivity.class);
                 startActivity(i);
             }
         });
